1401d6c11e542d8dd22425678edb919f32324183
Gabrielius Mickevičius <gabrielius.m@gmail.com>
2025-10-01 22:12:45 +0300

    Add PartitionFs

--- README.md
+++ README.md
@@ -10,30 +10,15 @@ import (
 	"io/fs"
 
 	aferoguestfs "github.com/gaboose/afero-guestfs"
-	"github.com/gaboose/afero-guestfs/libguestfs.org/guestfs"
 	"github.com/spf13/afero"
 )
 
 func main() {
-	g, err := guestfs.Create()
+	gfs, err := aferoguestfs.OpenPartitionFs("disk.img", "/dev/sda2")
 	if err != nil {
 		panic(err)
 	}
-	defer g.Close()
-
-	if err := g.Add_drive("diskimage.img", nil); err != nil {
-		panic(err)
-	}
-
-	if err := g.Launch(); err != nil {
-		panic(err)
-	}
-
-	if err := g.Mount("/dev/sda2", "/"); err != nil {
-		panic(err)
-	}
-
-	gfs := aferoguestfs.New(g)
+	defer gfs.Close()
 
 	afero.Walk(gfs, "/home", func(path string, info fs.FileInfo, err error) error {
 		fmt.Println(path)
---
+++ open.go
@@ -0,0 +1,49 @@
+package aferoguestfs
+
+import (
+	"fmt"
+
+	"github.com/gaboose/afero-guestfs/libguestfs.org/guestfs"
+)
+
+// PartitionFs is a utility type for opening a partition in a disk image.
+type PartitionFs struct {
+	*Fs
+	inner *guestfs.Guestfs
+}
+
+// OpenPartitionFs opens a new partition.
+// It takes a path to an image file and a partition device.
+func OpenPartitionFs(image string, partition string) (*PartitionFs, error) {
+	g, err := guestfs.Create()
+	if err != nil {
+		return nil, fmt.Errorf("create failed: %w", err)
+	}
+
+	if err := g.Add_drive(image, nil); err != nil {
+		g.Close()
+		return nil, fmt.Errorf("add drive failed: %w", err)
+	}
+
+	if err := g.Launch(); err != nil {
+		g.Close()
+		return nil, fmt.Errorf("launch failed: %w", err)
+	}
+
+	if err := g.Mount(partition, "/"); err != nil {
+		g.Close()
+		return nil, fmt.Errorf("failed to mount partition %s: %w", partition, err)
+	}
+
+	return &PartitionFs{Fs: New(g), inner: g}, nil
+}
+
+func (p *PartitionFs) Close() error {
+	if err := p.inner.Umount_all(); err != nil {
+		return fmt.Errorf("umount all failed: %w", err)
+	}
+	if err := p.inner.Close(); err != nil {
+		return fmt.Errorf("guestfs close failed: %w", err)
+	}
+	return nil
+}
